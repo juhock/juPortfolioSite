d0f632cf775776cf3cdda9bbf87cd7ef880f6afa
Julia Hocken <ju.hocken@gmail.com>
2024-01-30 12:29:31 -0500

    navbar added

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <ul className="NavbarItems">
+        <p>Home</p>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </ul>
+    </>
+  );
+}
+
+export default Navbar;
