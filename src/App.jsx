6e52616caf167932f0a1b020a0d1583e2f4f561b
Julia Hocken <ju.hocken@gmail.com>
2024-01-31 18:38:15 -0500

    changes to contact section links and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import { useState } from "react";
-import logo1 from "./assets/logo1.png";
+import profile from "./assets/profile2.png";
+import emailIcon from "./assets/emailIcon.png";
+import linkedIcon from "./assets/linkedin.png";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Welcome</h1>
-      <div className="card"></div>
-      <p>About Me </p>
-    </>
+    <div className="info">
+      <div className="aboutSection">
+        <img src={profile} alt="Picture of Julia Hocken" id="profilePicture" />
+        <div className="aboutText">
+          <h1>About Me </h1>
+          <p>
+            Welcome to my portfolio page! My name's Julia, and I'm a web
+            developer passionate about making a positive impact in accessibility
+            through technology.
+          </p>
+          <p>
+            I hold a BA and MA in Psychology, but decided to pursue web
+            development through Fullstack Academy's Coding Bootcamp.
+          </p>
+          <p>
+            Currently, I'm open to all job/project inquiries, so if there's
+            something you'd like to see come to life, please reach out!{" "}
+          </p>
+        </div>
+      </div>
+      <div className="projectsSection">
+        <h1>Projects</h1>
+        <p>My projects</p>
+      </div>
+      <div className="contactSection">
+        <h1>Contact</h1>
+        <p>
+          Feel free to contact me via email ju.hocken@gmail.com or connect
+          through LinkedIn.
+        </p>
+        <div className="contactIcons">
+          <ul className="links">
+            <div className="contactIcons">
+              <a href="mailto:ju.hocken@gmail.com">
+                <img src={emailIcon} alt="Email Icon" className="miniIcons" />
+                <li>Email</li>
+              </a>
+            </div>
+            <div className="contactIcons">
+              <a href="https://www.linkedin.com/in/julia-hocken/">
+                <img
+                  src={linkedIcon}
+                  alt="LinkedIn Icon"
+                  className="miniIcons"
+                />
+                <li>LinkedIn</li>
+              </a>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
