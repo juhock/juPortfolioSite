fe23abe873de639baea7037b10b3085b9e9f071e
Julia Hocken <ju.hocken@gmail.com>
2024-02-22 16:04:48 -0500

    form submission should work now

--- src/App.jsx
+++ src/App.jsx
@@ -28,31 +28,33 @@ function App() {
         </div>
       </div>
       <div className="projectsSection">
-        <h1>Projects</h1>
-        <a href="https://helpharbor.onrender.com/">
-          <img
-            src={helpHarborIcon}
-            alt="Help Harbor Icon"
-            className="miniIcons"
-          />
-          <li>Help Harbor</li>
-        </a>
+        <h1 className="titles">Recent Projects</h1>
+        <div className="allIcons">
+          <a href="https://helpharbor.onrender.com/">
+            <img
+              src={helpHarborIcon}
+              alt="Help Harbor Icon"
+              className="miniIcons"
+            />
+            <li>Help Harbor</li>
+          </a>
+        </div>
       </div>
       <div className="contactSection">
-        <h1>Contact</h1>
+        <h1 id="contactTitle">Contact Me</h1>
         <p>
-          Feel free to contact me via email ju.hocken@gmail.com or connect
-          through LinkedIn.
+          You can reach me via email ju.hocken@gmail.com, connect with me
+          through LinkedIn, or fill out the form below to send me a message.
         </p>
-        <div className="contactIcons">
+        <div>
           <ul className="links">
-            <div className="contactIcons">
+            <div className="allIcons">
               <a href="mailto:ju.hocken@gmail.com">
                 <img src={emailIcon} alt="Email Icon" className="miniIcons" />
                 <li>Email</li>
               </a>
             </div>
-            <div className="contactIcons">
+            <div className="allIcons">
               <a href="https://www.linkedin.com/in/julia-hocken/">
                 <img
                   src={linkedIcon}
@@ -63,6 +65,45 @@ function App() {
               </a>
             </div>
           </ul>
+        </div>
+
+        <div className="contact-container">
+          <form
+            action="https://api.web3forms.com/submit"
+            method="POST"
+            className="contact-left"
+          ></form>
+          <input
+            type="hidden"
+            name="access_key"
+            value="5e04e505-ddc6-4f57-8893-16f767395990"
+          ></input>
+          <div className="contact-left-title">
+            <h2>Get in touch</h2>
+            <hr></hr>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className="contact-inputs"
+              required
+            ></input>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="contact-inputs"
+              required
+            ></input>
+            <textarea
+              name="message"
+              placeholder="Your Message"
+              className="contact-inputs"
+              required
+            ></textarea>
+            <button type="submit">Submit</button>
+          </div>
+          <div className="contact-right"></div>
         </div>
       </div>
     </div>
