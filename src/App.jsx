291beb6b1b31c49aa1d2a52816ece56e2648613c
Julia Hocken <ju.hocken@gmail.com>
2024-03-05 20:44:21 -0500

    adjusted app jsx for better semantics

--- src/App.jsx
+++ src/App.jsx
@@ -27,36 +27,59 @@ function App() {
       <div className="projectsSection">
         <h1 id="projectTitle">Recent Projects</h1>
         <div className="icons">
-          <a href="https://helpharbor.onrender.com/">
-            <img
-              src={helpHarborIcon}
-              alt="Help Harbor Icon"
-              className="miniIcons"
-            />
-            <li>Help Harbor</li>
-          </a>
+          <ul className="links">
+            <li>
+              <div className="icons">
+                <a href="https://helpharbor.onrender.com/">
+                  <img
+                    src={helpHarborIcon}
+                    alt="Help Harbor Icon"
+                    className="iconPic"
+                  />
+                  <p>Help Harbor</p>
+                </a>
+              </div>
+            </li>
+            {/* <li>
+              <div className="icons">
+                <a href="https://helpharbor.onrender.com/">
+                  <img
+                    src={helpHarborIcon}
+                    alt="Help Harbor Icon"
+                    className="iconPic"
+                  />
+                  <p>Help Harbor</p>
+                </a>
+              </div>
+            </li> */}
+          </ul>
         </div>
       </div>
+
       <div className="contactSection">
         <h1 id="contactTitle">Contact Me</h1>
         <div>
           <ul className="links">
-            <div className="icons">
-              <a href="mailto:ju.hocken@gmail.com">
-                <img src={emailIcon} alt="Email Icon" className="miniIcons" />
-                <li>Email</li>
-              </a>
-            </div>
-            <div className="icons">
-              <a href="https://www.linkedin.com/in/julia-hocken/">
-                <img
-                  src={linkedIcon}
-                  alt="LinkedIn Icon"
-                  className="miniIcons"
-                />
-                <li>LinkedIn</li>
-              </a>
-            </div>
+            <li>
+              <div className="icons">
+                <a href="mailto:ju.hocken@gmail.com">
+                  <img src={emailIcon} alt="Email Icon" className="iconPic" />
+                </a>
+                <p>Email</p>
+              </div>
+            </li>
+            <li>
+              <div className="icons">
+                <a href="https://www.linkedin.com/in/julia-hocken/">
+                  <img
+                    src={linkedIcon}
+                    alt="LinkedIn Icon"
+                    className="iconPic"
+                  />
+                </a>
+                <p>LinkedIn</p>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
